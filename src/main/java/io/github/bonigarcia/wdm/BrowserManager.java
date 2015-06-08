6633e7153d984ebdb7a3f408c70a501adb3678ce
Boni Garcia <bgarcia@gsyc.es>
2015-06-08 23:52:59 +0200

    Bug-fix: Wrong chromedriver version downloaded (GitHub issue #1)

--- src/main/java/io/github/bonigarcia/wdm/BrowserManager.java
+++ src/main/java/io/github/bonigarcia/wdm/BrowserManager.java
@@ -109,14 +109,35 @@ public abstract class BrowserManager {
 						url.getFile().lastIndexOf(SEPARATOR));
 				if (latestVersion == null) {
 					latestVersion = currentVersion;
-					log.info("Latest driver version: {}", latestVersion);
+				}
+				if (versionCompare(currentVersion, latestVersion) > 0) {
+					latestVersion = currentVersion;
+					out.clear();
 				}
 				if (url.getFile().contains(latestVersion)) {
 					out.add(url);
 				}
 			}
 		}
+		log.info("Latest driver version: {}", latestVersion);
 		return out;
+	}
+
+	public Integer versionCompare(String str1, String str2) {
+		String[] vals1 = str1.split("\\.");
+		String[] vals2 = str2.split("\\.");
+		int i = 0;
+		while (i < vals1.length && i < vals2.length
+				&& vals1[i].equals(vals2[i])) {
+			i++;
+		}
+		if (i < vals1.length && i < vals2.length) {
+			int diff = Integer.valueOf(vals1[i]).compareTo(
+					Integer.valueOf(vals2[i]));
+			return Integer.signum(diff);
+		} else {
+			return Integer.signum(vals1.length - vals2.length);
+		}
 	}
 
 	public List<URL> getDriversFromXml(Architecture arch, URL driverUrl,
