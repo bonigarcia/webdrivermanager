df897a6240c6e05c3df671dc8400839fb0b5f486
Jan-Hendrik Peters <hennr@hennr.name>
2016-08-30 22:48:41 +0200

    skip operaTest on travis as the operadriver does not work in headless via xvfb

--- src/test/java/io/github/bonigarcia/wdm/test/OperaTest.java
+++ src/test/java/io/github/bonigarcia/wdm/test/OperaTest.java
@@ -15,6 +15,7 @@
 package io.github.bonigarcia.wdm.test;
 
 import static org.apache.commons.lang3.SystemUtils.IS_OS_LINUX;
+import static org.junit.Assume.assumeTrue;
 
 import java.io.File;
 
@@ -35,8 +36,14 @@ import io.github.bonigarcia.wdm.base.BaseBrowserTst;
  */
 public class OperaTest extends BaseBrowserTst {
 
+	static boolean headlessMode = "true".equals(System.getProperty("headless"));
+
 	@BeforeClass
 	public static void setupClass() {
+		if (headlessMode) {
+			validOS = false;
+			assumeTrue(false);
+		}
 		OperaDriverManager.getInstance().setup();
 	}
 
