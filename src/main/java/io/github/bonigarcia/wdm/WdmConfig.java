1b3d2d1912087474fa060c7c883938795168f407
Boni Garcia <bgarcia@gsyc.es>
2017-11-19 18:58:07 +0100

    Revert "Stop using environmental variables to read config keys"
    This reverts commit e44f718746f135f73ab070711df47330a9eddcb2.

--- src/main/java/io/github/bonigarcia/wdm/WdmConfig.java
+++ src/main/java/io/github/bonigarcia/wdm/WdmConfig.java
@@ -44,8 +44,16 @@ public class WdmConfig {
     }
 
     public static String getString(String key) {
-        return key.equals("") ? ""
-                : WdmConfig.getInstance().conf.getString(key);
+        String value = "";
+        if (!key.equals("")) {
+            // dots are not allowed in POSIX environmental variables
+            value = System.getenv(key.replace(".", "_"));
+            if (value == null) {
+                value = WdmConfig.getInstance().conf.getString(key);
+            }
+
+        }
+        return value;
     }
 
     public static int getInt(String key) {
