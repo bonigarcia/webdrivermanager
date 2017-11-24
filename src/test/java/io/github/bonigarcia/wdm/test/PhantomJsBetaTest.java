e2c749a858b1635e7ebb5536fbe0420ae8f2c970
Boni Garcia <bgarcia@gsyc.es>
2017-11-24 03:18:20 +0100

    Include PhantomJS test forcing beta versions

---
+++ src/test/java/io/github/bonigarcia/wdm/test/PhantomJsBetaTest.java
@@ -0,0 +1,44 @@
+/*
+ * (C) Copyright 2016 Boni Garcia (http://bonigarcia.github.io/)
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+package io.github.bonigarcia.wdm.test;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+
+import io.github.bonigarcia.wdm.PhantomJsDriverManager;
+import io.github.bonigarcia.wdm.base.BaseBrowserTst;
+
+/**
+ * Test with PhatomJS.
+ *
+ * @author Boni Garcia (boni.gg@gmail.com)
+ * @since 1.4.0
+ */
+@Ignore
+public class PhantomJsBetaTest extends BaseBrowserTst {
+
+    @BeforeClass
+    public static void setupClass() {
+        PhantomJsDriverManager.getInstance().useBetaVersions().setup();
+    }
+
+    @Before
+    public void setupTest() {
+        driver = new PhantomJSDriver();
+    }
+
+}
