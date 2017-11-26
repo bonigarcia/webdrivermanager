502ec45a281d8f123cd34a6356216806d91daab9
Boni Garcia <bgarcia@gsyc.es>
2017-11-26 18:59:37 +0100

    Proper teardown in taobao mirror test

--- src/test/java/io/github/bonigarcia/wdm/test/TaobaoTest.java
+++ src/test/java/io/github/bonigarcia/wdm/test/TaobaoTest.java
@@ -15,25 +15,19 @@
 package io.github.bonigarcia.wdm.test;
 
 import static java.lang.System.setProperty;
-import static java.util.Arrays.asList;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.assertThat;
 
 import java.lang.reflect.Method;
+import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Collection;
 
+import org.junit.AfterClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameter;
-import org.junit.runners.Parameterized.Parameters;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.BrowserManager;
+import io.github.bonigarcia.wdm.ChromeDriverManager;
 import io.github.bonigarcia.wdm.EdgeDriverManager;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import io.github.bonigarcia.wdm.WebDriverManagerException;
 
 /**
@@ -42,32 +36,18 @@ import io.github.bonigarcia.wdm.WebDriverManagerException;
  * @author Boni Garcia (boni.gg@gmail.com)
  * @since 1.6.1
  */
-@RunWith(Parameterized.class)
 public class TaobaoTest {
-
-    @Parameter(0)
-    public Class<? extends WebDriver> driverClass;
-
-    @Parameter(1)
-    public String taobaoUrl;
-
-    @Parameters(name = "{index}: {0}")
-    public static Collection<Object[]> data() {
-        return asList(new Object[][] { { ChromeDriver.class,
-                "http://npm.taobao.org/mirrors/chromedriver/2.33/" } });
-    }
 
     @Test
     public void testTaobao() throws Exception {
-        setProperty("wdm.chromeDriverTaobaoUrl", taobaoUrl);
-
-        BrowserManager browserManager = WebDriverManager
-                .getInstance(driverClass);
-        browserManager.useTaobaoMirror().setup();
+        setProperty("wdm.chromeDriverTaobaoUrl",
+                "http://npm.taobao.org/mirrors/chromedriver/2.33/");
+        BrowserManager chromeDriver = ChromeDriverManager.getInstance();
+        chromeDriver.useTaobaoMirror().setup();
 
         Method method = BrowserManager.class.getDeclaredMethod("getDriverUrl");
         method.setAccessible(true);
-        URL driverUrl = (URL) method.invoke(browserManager);
+        URL driverUrl = (URL) method.invoke(chromeDriver);
 
         assertThat(driverUrl.toString(), containsString("taobao.org"));
     }
@@ -75,6 +55,14 @@ public class TaobaoTest {
     @Test(expected = WebDriverManagerException.class)
     public void testTaobaoException() {
         EdgeDriverManager.getInstance().useTaobaoMirror().setup();
+    }
+
+    @AfterClass
+    public static void teardown() throws MalformedURLException {
+        ChromeDriverManager.getInstance().driverRepositoryUrl(
+                new URL("https://chromedriver.storage.googleapis.com/"));
+        EdgeDriverManager.getInstance().driverRepositoryUrl(new URL(
+                "https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/"));
     }
 
 }
