66aceb9d5a5f4ef892a990ce32e0a423df65b73b
PavelAlennikov <palennikov@gmail.com>
2018-01-26 14:21:24 +0700

    change logger level to INFO. delete Main class. delete logger file appender

--- src/main/java/Main.java
+++
@@ -1,7 +0,0 @@
-import io.github.bonigarcia.wdm.ChromeDriverManager;
-
-public class Main {
-    public static void main(String[] args) {
-        ChromeDriverManager.getInstance().setup();
-    }
-}
