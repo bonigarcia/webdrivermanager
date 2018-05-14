eaf23a27c9d3622907f29cc129ba22f6494b00ba
Boni Garcia <bgarcia@gsyc.es>
2018-05-14 12:22:55 +0200

    Extract use beta version flag to class parameter (issue #206)

--- src/main/java/io/github/bonigarcia/wdm/WebDriverManager.java
+++ src/main/java/io/github/bonigarcia/wdm/WebDriverManager.java
@@ -94,6 +94,7 @@ public abstract class WebDriverManager {
     protected static Map<DriverManagerType, WebDriverManager> instanceMap = new EnumMap<>(
             DriverManagerType.class);
     protected static Config config;
+    protected boolean useBeta = config().isUseBetaVersions();
     protected HttpClient httpClient;
     protected Downloader downloader;
     protected UrlFilter urlFilter;
@@ -680,8 +681,7 @@ public abstract class WebDriverManager {
     }
 
     protected void handleDriver(URL url, String driver, List<URL> out) {
-        if (!config().isUseBetaVersions()
-                && url.getFile().toLowerCase().contains("beta")) {
+        if (!useBeta && url.getFile().toLowerCase().contains("beta")) {
             return;
         }
         if (url.getFile().contains(driver)) {
@@ -912,6 +912,7 @@ public abstract class WebDriverManager {
         listVersions = null;
         versionToDownload = null;
         forcedArch = false;
+        useBeta = config().isUseBetaVersions();
     }
 
     public static void main(String[] args) {
