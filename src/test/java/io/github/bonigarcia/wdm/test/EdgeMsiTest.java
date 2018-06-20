01a3f525b581290e1a66aa2c815c26ae0e985008
Boni Garcia <bgarcia@gsyc.es>
2018-06-20 15:24:10 +0200

    Simplify Edge test

--- src/test/java/io/github/bonigarcia/wdm/test/EdgeMsiTest.java
+++ src/test/java/io/github/bonigarcia/wdm/test/EdgeMsiTest.java
@@ -16,26 +16,15 @@
  */
 package io.github.bonigarcia.wdm.test;
 
-import static io.github.bonigarcia.wdm.DriverManagerType.EDGE;
 import static io.github.bonigarcia.wdm.WebDriverManager.edgedriver;
 import static java.lang.invoke.MethodHandles.lookup;
-import static org.apache.commons.io.FileUtils.cleanDirectory;
-import static org.apache.commons.lang3.SystemUtils.IS_OS_WINDOWS;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assume.assumeFalse;
-import static org.junit.Assume.assumeTrue;
 import static org.slf4j.LoggerFactory.getLogger;
 
 import java.io.File;
-import java.io.IOException;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
-
-import io.github.bonigarcia.wdm.Downloader;
-import io.github.bonigarcia.wdm.WebDriverManagerException;
 
 /**
  * Test with Microsoft Edge forcing to extract MSI file.
@@ -47,21 +36,8 @@ public class EdgeMsiTest {
 
     final Logger log = getLogger(lookup().lookupClass());
 
-    @Before
-    @After
-    public void cleanCache() throws IOException {
-        cleanDirectory(new File(new Downloader(EDGE).getTargetPath()));
-    }
-
-    @Test(expected = WebDriverManagerException.class)
-    public void testMsiOutWindows() {
-        assumeFalse(IS_OS_WINDOWS);
-        edgedriver().version("2.10586").setup();
-    }
-
     @Test
     public void testMsiInWindows() {
-        assumeTrue(IS_OS_WINDOWS);
         edgedriver().version("2.10586").setup();
         File binary = new File(edgedriver().getBinaryPath());
         log.debug("Edge driver {}", binary);
