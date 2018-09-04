6ba28b4adef5f3f9a6ca2b2cfdbdadf66d87b323
Konstantin Maltsev <mkypers@gmail.com>
2018-09-04 11:30:03 +0700

    add release profile

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,4 +6,4 @@ else
     echo "not on a tag -> keep snapshot version in pom.xml"
 fi
 
-mvn clean deploy --settings .travis/settings.xml -DskipTests=true -B -U
+mvn clean deploy --settings .travis/settings.xml -DskipTests=true -Drelease=true -B -U
