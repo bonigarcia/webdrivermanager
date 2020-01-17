c7caf5d727a8764d9817cada1f0b06d88519bb55
Boni Garcia <bgarcia@gsyc.es>
2020-01-17 23:10:48 +0100

    Order files in cache using version comparator

--- src/main/java/io/github/bonigarcia/wdm/WebDriverManager.java
+++ src/main/java/io/github/bonigarcia/wdm/WebDriverManager.java
@@ -446,7 +446,7 @@ public abstract class WebDriverManager {
                 }
             }
             log.trace("Version list before sorting {}", versions);
-            sort(versions, new VersionComparator());
+            sort(versions, new VersionComparator<>());
             return versions;
         } catch (IOException e) {
             throw new WebDriverManagerException(e);
@@ -915,8 +915,10 @@ public abstract class WebDriverManager {
     }
 
     protected List<File> getFilesInCache() {
-        return (List<File>) listFiles(new File(downloader.getTargetPath()),
-                null, true);
+        List<File> listFiles = (List<File>) listFiles(
+                new File(downloader.getTargetPath()), null, true);
+        sort(listFiles, new VersionComparator<>());
+        return listFiles;
     }
 
     protected List<URL> removeFromList(List<URL> list, String version) {
