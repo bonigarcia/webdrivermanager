1f6ff3ad968628c30a1eaa7fc8fb51006bf2d967
Boni Garcia <bgarcia@gsyc.es>
2020-02-05 10:58:43 +0100

    Update versions in Edge (read) test

--- src/test/java/io/github/bonigarcia/wdm/test/EdgeReadVersionTest.java
+++ src/test/java/io/github/bonigarcia/wdm/test/EdgeReadVersionTest.java
@@ -42,8 +42,8 @@ public class EdgeReadVersionTest {
     @Test
     public void edgeVersionsTest() {
         String[] expectedVersions = { "1.10240", "2.10586", "3.14393",
-                "4.15063", "5.16299", "6.17134", "75.0.139.20", "76.0.183.0",
-                "77.0.237.0" };
+                "4.15063", "5.16299", "6.17134", "79.0.313.0", "80.0.361.45",
+                "81.0.413.0" };
         List<String> versions = WebDriverManager.edgedriver().getVersions();
 
         log.debug("Expected edge versions: {}",
