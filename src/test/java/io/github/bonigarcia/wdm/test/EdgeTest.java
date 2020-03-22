d84f940d53a823e524579e8bad7cf2d52b47b95d
eliasnogueira <elias.nogueira@gmail.com>
2020-03-22 13:45:24 +0100

    add mac verification on setup

--- src/test/java/io/github/bonigarcia/wdm/test/EdgeTest.java
+++ src/test/java/io/github/bonigarcia/wdm/test/EdgeTest.java
@@ -16,6 +16,7 @@
  */
 package io.github.bonigarcia.wdm.test;
 
+import static org.apache.commons.lang3.SystemUtils.IS_OS_MAC;
 import static org.apache.commons.lang3.SystemUtils.IS_OS_WINDOWS;
 import static org.junit.Assume.assumeTrue;
 
@@ -36,7 +37,7 @@ public class EdgeTest extends BrowserTestParent {
 
     @BeforeClass
     public static void setupClass() {
-        assumeTrue(IS_OS_WINDOWS);
+        assumeTrue(IS_OS_WINDOWS || IS_OS_MAC);
         WebDriverManager.edgedriver().setup();
     }
 
