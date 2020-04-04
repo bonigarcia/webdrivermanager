d0e1c15f39501e75e402c557a807975192be71f5
Boni García <boni.garcia@uc3m.es>
2020-04-04 11:26:26 +0200

    Fix Edge Mac test

--- src/test/java/io/github/bonigarcia/wdm/test/EdgeMacVersionTest.java
+++ src/test/java/io/github/bonigarcia/wdm/test/EdgeMacVersionTest.java
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 2015 Boni Garcia (http://bonigarcia.github.io/)
+ * (C) Copyright 2020 Boni Garcia (http://bonigarcia.github.io/)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,15 +18,16 @@ package io.github.bonigarcia.wdm.test;
 
 import static io.github.bonigarcia.wdm.OperatingSystem.MAC;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import io.github.bonigarcia.wdm.base.VersionTestParent;
 import org.junit.Before;
 import org.openqa.selenium.edge.EdgeDriver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.base.VersionTestParent;
+
 /**
- * Test asserting Edge driver versions on MacOSX.
- * This is a separate class because the Win versus MacOSX versions aren't the same
- * Please take a look at https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/
+ * Test asserting Edge driver versions on MacOSX. This is a separate class
+ * because the Win versus MacOSX versions aren't the same Please take a look at
+ * https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/
  *
  * @author Elias Nogueira (elias.nogueira@gmail.com)
  * @since 3.8.2
@@ -37,10 +38,6 @@ public class EdgeMacVersionTest extends VersionTestParent {
     public void setup() {
         browserManager = WebDriverManager.getInstance(EdgeDriver.class);
         os = MAC;
-        specificVersions = new String[] {
-            "80.0.361.62", "80.0.361.66", "80.0.361.69",
-            "81.0.416.28", "81.0.416.31", "81.0.416.34",
-            "82.0.456.0", "82.0.457.0", "82.0.458.0"
-        };
+        specificVersions = new String[] { "81.0.416.45", "82.0.459.1" };
     }
 }
