e88638bd6fde68146ba8cd2095403c0d86285636
Boni García <boni.garcia@uc3m.es>
2020-04-25 16:53:33 +0200

    Parameterize force download test to use multiple managers

--- src/test/java/io/github/bonigarcia/wdm/test/ForceDownloadTest.java
+++ src/test/java/io/github/bonigarcia/wdm/test/ForceDownloadTest.java
@@ -16,11 +16,24 @@
  */
 package io.github.bonigarcia.wdm.test;
 
+import static java.util.Arrays.asList;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
-import org.junit.Test;
+import java.util.Collection;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
+
+import io.github.bonigarcia.wdm.OperatingSystem;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 /**
@@ -29,14 +42,29 @@ import io.github.bonigarcia.wdm.WebDriverManager;
  * @author Boni Garcia (boni.gg@gmail.com)
  * @since 2.1.1
  */
+@RunWith(Parameterized.class)
 public class ForceDownloadTest {
 
+    static final int TIMEOUT = 20;
+    static final OperatingSystem OS = OperatingSystem.WIN;
+
+    @Parameter
+    public Class<? extends WebDriver> driverClass;
+
+    @Parameters(name = "{index}: {0}")
+    public static Collection<Object[]> data() {
+        return asList(new Object[][] { { ChromeDriver.class },
+                { FirefoxDriver.class }, { OperaDriver.class },
+                { EdgeDriver.class } });
+    }
+
     @Test
-    public void test() {
-        WebDriverManager.chromedriver().forceDownload().avoidAutoVersion()
-                .timeout(20).setup();
-        assertThat(WebDriverManager.chromedriver().getBinaryPath(),
-                notNullValue());
+    public void testLatestChrome() {
+        WebDriverManager driverManager = WebDriverManager
+                .getInstance(driverClass);
+        driverManager.forceDownload().avoidAutoVersion().timeout(TIMEOUT)
+                .operatingSystem(OS).setup();
+        assertThat(driverManager.getBinaryPath(), notNullValue());
     }
 
 }
