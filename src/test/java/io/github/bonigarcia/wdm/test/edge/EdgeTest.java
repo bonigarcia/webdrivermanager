53fb7c0e678db78f5f43f217cdcfb7d5d2935a9c
Boni García <boni.garcia@uc3m.es>
2021-01-14 16:30:08 +0100

    Run Edge test also in Linux

--- src/test/java/io/github/bonigarcia/wdm/test/edge/EdgeTest.java
+++ src/test/java/io/github/bonigarcia/wdm/test/edge/EdgeTest.java
@@ -16,10 +16,6 @@
  */
 package io.github.bonigarcia.wdm.test.edge;
 
-import static org.apache.commons.lang3.SystemUtils.IS_OS_MAC;
-import static org.apache.commons.lang3.SystemUtils.IS_OS_WINDOWS;
-import static org.junit.Assume.assumeTrue;
-
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -37,7 +33,6 @@ public class EdgeTest extends BrowserTestParent {
 
     @BeforeClass
     public static void setupClass() {
-        assumeTrue(IS_OS_WINDOWS || IS_OS_MAC);
         WebDriverManager.edgedriver().setup();
     }
 
