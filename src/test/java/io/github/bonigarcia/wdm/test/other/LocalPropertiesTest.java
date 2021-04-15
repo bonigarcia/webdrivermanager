aa4318566ffe7ac9d7a6a1d28c1a5057e4640513
Boni García <boni.garcia@uc3m.es>
2021-04-15 12:35:14 +0200

    Tests with custom configuration for versions and commands properties

---
+++ src/test/java/io/github/bonigarcia/wdm/test/other/LocalPropertiesTest.java
@@ -0,0 +1,69 @@
+/*
+ * (C) Copyright 2021 Boni Garcia (http://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia.wdm.test.other;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.junit.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+/**
+ * Tests with custom configuration for versions and commands properties.
+ *
+ * @author Boni Garcia (boni.gg@gmail.com)
+ * @since 4.4.1
+ */
+public class LocalPropertiesTest {
+
+    @Test
+    public void testCustomUrl() throws MalformedURLException {
+        File dot = new File(".");
+        URL versionsPropertiesUrl = new URL("file://" + dot.getAbsolutePath()
+                + "/src/main/resources/versions.properties");
+        URL commandsPropertiesUrl = new URL("file://" + dot.getAbsolutePath()
+                + "/src/main/resources/commands.properties");
+        WebDriverManager.chromedriver().clearResolutionCache()
+                .avoidReadReleaseFromRepository()
+                .versionsPropertiesUrl(versionsPropertiesUrl)
+                .commandsPropertiesUrl(commandsPropertiesUrl).setup();
+
+        assertDriverPath();
+    }
+
+    @Test
+    public void testLocalFirst() {
+        WebDriverManager.chromedriver().clearResolutionCache()
+                .avoidReadReleaseFromRepository()
+                .useLocalCommandsPropertiesFirst()
+                .useLocalVersionsPropertiesFirst().setup();
+
+        assertDriverPath();
+    }
+
+    private void assertDriverPath() {
+        String driverPath = WebDriverManager.chromedriver()
+                .getDownloadedDriverPath();
+        File driver = new File(driverPath);
+        assertTrue(driver.exists());
+    }
+
+}
