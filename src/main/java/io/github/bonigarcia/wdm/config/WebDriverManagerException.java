08f44718761be21db6770b1e419ff9af065fb0d7
Boni García <boni.garcia@uc3m.es>
2021-08-13 13:51:43 +0200

    Use https in header URL

--- src/main/java/io/github/bonigarcia/wdm/config/WebDriverManagerException.java
+++ src/main/java/io/github/bonigarcia/wdm/config/WebDriverManagerException.java
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 2017 Boni Garcia (http://bonigarcia.github.io/)
+ * (C) Copyright 2017 Boni Garcia (https://bonigarcia.github.io/)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
