4fd2c3a7b9e4b2b19361e5d6d67b876fa466fabf
Boni García <boni.garcia@uc3m.es>
2021-10-17 01:16:27 +0200

    Mount tmpfs always

--- src/main/java/io/github/bonigarcia/wdm/docker/DockerService.java
+++ src/main/java/io/github/bonigarcia/wdm/docker/DockerService.java
@@ -25,7 +25,6 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.Locale.ROOT;
 import static java.util.stream.Collectors.toList;
 import static org.apache.commons.lang.SystemUtils.IS_OS_LINUX;
-import static org.apache.commons.lang.SystemUtils.IS_OS_WINDOWS;
 import static org.slf4j.LoggerFactory.getLogger;
 
 import java.io.IOException;
@@ -570,14 +569,11 @@ public class DockerService {
 
         // mounts
         List<Mount> mounts = new ArrayList<>();
-        if (!IS_OS_WINDOWS) {
-            Mount tmpfsMount = new Mount()
-                    .withTmpfsOptions(new TmpfsOptions()
-                            .withSizeBytes(config.getDockerMemSizeBytes(
-                                    config.getDockerTmpfsSize())))
-                    .withTarget(config.getDockerTmpfsMount());
-            mounts.add(tmpfsMount);
-        }
+        Mount tmpfsMount = new Mount()
+                .withTmpfsOptions(new TmpfsOptions().withSizeBytes(config
+                        .getDockerMemSizeBytes(config.getDockerTmpfsSize())))
+                .withTarget(config.getDockerTmpfsMount());
+        mounts.add(tmpfsMount);
 
         // envs
         List<String> envs = new ArrayList<>();
