57d4c4b183010bd74ede6d6164188a964e2d1407
Boni Garcia <boni.garcia@uc3m.es>
2023-04-21 16:12:55 +0200

    Disable Docker Opera test

--- src/test/java/io/github/bonigarcia/wdm/test/docker/DockerOperaTest.java
+++ src/test/java/io/github/bonigarcia/wdm/test/docker/DockerOperaTest.java
@@ -20,11 +20,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
+@Disabled
 class DockerOperaTest {
 
     WebDriver driver;
