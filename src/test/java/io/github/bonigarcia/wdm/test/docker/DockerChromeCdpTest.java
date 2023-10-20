6695d749009ac4fccd6de083f55af30fe27b0fa7
Boni Garcia <boni.garcia@uc3m.es>
2023-10-20 14:53:58 +0200

    Include Docker test using CDP

---
+++ src/test/java/io/github/bonigarcia/wdm/test/docker/DockerChromeCdpTest.java
@@ -0,0 +1,86 @@
+/*
+ * (C) Copyright 2023 Boni Garcia (https://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia.wdm.test.docker;
+
+import static io.github.bonigarcia.wdm.WebDriverManager.isDockerAvailable;
+import static java.lang.invoke.MethodHandles.lookup;
+import static org.assertj.core.api.Assumptions.assumeThat;
+import static org.slf4j.LoggerFactory.getLogger;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.JavascriptException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.HasDevTools;
+import org.openqa.selenium.devtools.events.ConsoleEvent;
+import org.openqa.selenium.remote.Augmenter;
+import org.slf4j.Logger;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+@Disabled
+class DockerChromeCdpTest {
+
+    static final Logger log = getLogger(lookup().lookupClass());
+
+    WebDriver driver;
+    DevTools devTools;
+    WebDriverManager wdm = WebDriverManager.chromedriver().browserInDocker();
+
+    @BeforeEach
+    void setupTest() {
+        assumeThat(isDockerAvailable()).isTrue();
+        driver = new Augmenter().augment(wdm.create());
+        devTools = ((HasDevTools) driver).getDevTools();
+        devTools.createSession();
+    }
+
+    @AfterEach
+    void teardown() {
+        devTools.close();
+        wdm.quit();
+    }
+
+    @Test
+    void test() throws Exception {
+        CompletableFuture<ConsoleEvent> futureEvents = new CompletableFuture<>();
+        devTools.getDomains().events()
+                .addConsoleListener(futureEvents::complete);
+
+        CompletableFuture<JavascriptException> futureJsExc = new CompletableFuture<>();
+        devTools.getDomains().events()
+                .addJavascriptExceptionListener(futureJsExc::complete);
+
+        driver.get(
+                "https://bonigarcia.dev/selenium-webdriver-java/console-logs.html");
+
+        ConsoleEvent consoleEvent = futureEvents.get(5, TimeUnit.SECONDS);
+        log.debug("ConsoleEvent: {} {} {}", consoleEvent.getTimestamp(),
+                consoleEvent.getType(), consoleEvent.getMessages());
+
+        JavascriptException jsException = futureJsExc.get(5, TimeUnit.SECONDS);
+        log.debug("JavascriptException: {} {}", jsException.getMessage(),
+                jsException.getSystemInformation());
+    }
+
+}
